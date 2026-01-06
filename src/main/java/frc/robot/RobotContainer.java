57a8e46dbba5cb0bb132d9e6335a15340293a206
notxander4211 <xandercode2@gmail.com>
2026-01-05 16:39:10 -0800

    changed robotcontainter

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -17,6 +17,7 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
  * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
+ * ersgkhegei
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
